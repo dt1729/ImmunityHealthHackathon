3745e4c90e15595d06a020dd08a0557b723c6919
dt1729 <divyatiwari1112@gmail.com>
2020-08-05 13:56:06 +0530

    Label Detection and CSV save

--- SegmentationBasedhumandetection/Mainfile.py
+++ SegmentationBasedhumandetection/Mainfile.py
@@ -4,6 +4,9 @@ import os
 from PIL import Image
 from DeepLabV3 import DeepLab
 import get_dataset_colormap
+import json 
+
+
 
 cap = cv2.VideoCapture(0)
 model_dir = '/Users/dt/Desktop/CodesTemp/ImmunityHealth/ImmunityHealthHackathon/SegmentationBasedhumandetection'
@@ -11,6 +14,15 @@ _TARBALL_NAME = 'deeplab_model.tar.gz'
 _FROZEN_GRAPH = 'frozen_inference_graph'
 download_path = os.path.join(model_dir, _TARBALL_NAME)
 model = DeepLab(download_path)
+
+
+LABEL_NAMES = np.asarray([
+    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+])
+
+
 def referenceBackground():
     while (True):
         ret1, frame1 = cap.read()
@@ -28,24 +40,43 @@ def referenceBackground():
             seg_image = get_dataset_colormap.label_to_color_image(
                 seg_map, get_dataset_colormap.get_pascal_name()).astype(np.uint8)
             # cv2.imshow('background',seg_map)
-            print(seg_image)
+            unique_labels,label_counts = np.unique(seg_map, return_counts=True)
+            in_this_pic = LABEL_NAMES[unique_labels]
+            grouped_label_counts  = dict(zip(in_this_pic,label_counts))
+            # saving semantic data as json
+            save_json(grouped_label_counts,'reference')
+            # loadcheck 
+            open_json('reference.txt')
             cv2.imwrite('/Users/dt/Desktop/CodesTemp/ImmunityHealth/ImmunityHealthHackathon/SegmentationBasedhumandetection/referenceBackgroundSegmented.jpg', seg_image)
             break
     cv2.destroyAllWindows()
     return seg_image,seg_map
 
-# def postureCorrection():
-#     ref_Seg = cv2.imread('/Users/dt/Desktop/CodesTemp/ImmunityHealth/ImmunityHealthHackathon/SegmentationBasedhumandetection/referenceBackgroundSegmented.jpg')
-#     while(cap.isOpened()):
 
 
+def save_json(grouped_label_counts,filename):
+    data = grouped_label_counts
+    with open(filename + '.txt', 'w') as outfile:
+        json.dump(data, outfile)
 
+def open_json(filename):
+    data = json.load(filename)
+    print(data);
 
+def postureCorrection():
+    ref_Seg = cv2.imread('/Users/dt/Desktop/CodesTemp/ImmunityHealth/ImmunityHealthHackathon/SegmentationBasedhumandetection/referenceBackgroundSegmented.jpg')
+    while(cap.isOpened()):
+        pass
+    pass
 
 
 
 if __name__ == "__main__":
-    seg_img,seg_map =  referenceBackground()
+    reference = input("have you taken reference before? Y or N")
+    if reference == "N":
+        seg_img,seg_map =  referenceBackground()
+    else:
+        segnums = open_json('reference.txt')
     cap.release()
     
 
